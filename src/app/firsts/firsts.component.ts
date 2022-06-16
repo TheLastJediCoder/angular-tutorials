ccbf535ed3b9005a3116778fdd914052345c145a
Harsh Parecha <harshparecha@gmail.coms>
2022-06-16 12:12:01 +0530

    Basic of Components

---
+++ src/app/firsts/firsts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firsts',
+  templateUrl: './firsts.component.html',
+  styleUrls: ['./firsts.component.css']
+})
+export class FirstsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
